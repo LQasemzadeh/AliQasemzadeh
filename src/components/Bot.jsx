b8f5c1ec852d69eb7870e46813c00ce995eb61fa
lqasemzadeh <lqasemzadeh@gmail.com>
2024-07-15 10:44:19 +0330

    Update Bot component design
    The Bot component has been updated to include an icon from react-icons. A set of additional styles were applied to make it more visually engaging and distinct on the page. The changes resulted in an animated, cyan-colored, round chat bubble on the bottom right side of the screen.

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { IoChatbubbleEllipses } from "react-icons/io5";
+
+
 
 const Bot = () => {
-  return <div>Bot</div>;
+  return (
+      <div className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-cyan-600
+      w-16 h-16 flex items-center justify-center rounded-full animate-bounce">
+    <IoChatbubbleEllipses />
+      </div>
+  );
 };
 
 export default Bot;
