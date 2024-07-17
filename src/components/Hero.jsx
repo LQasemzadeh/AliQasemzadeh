57507b4d77e8d9b194846926bc0194cdba1a06b7
lqasemzadeh <lqasemzadeh@gmail.com>
2024-07-17 09:41:07 +0330

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import hero from "../../src/assets/images/hero.png";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+      <section
+      id="home"
+      className="min-h-screen flex py-10 md:flex-row flex-col items-center">
+    <div className="flex-1 flex items-center justify-center h-full">
+      <img src={hero} alt="" className="md:w-11/12 h-full object-cover" />
+    </div>
+        <div className="flex-1">
+
+        </div>
+  </section>
+      );
 };
 
 export default Hero;
